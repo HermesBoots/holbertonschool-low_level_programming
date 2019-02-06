1dfae532dc76199895f3de943317e630b6bc73ba
Sam Hermes <701@holbertonschool.com>
2019-02-06 15:31:37 +0000

    fixed undeclared name in tenth task.

--- 101-quote.c
+++ 101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(output, 1, sizeof(text) - sizeof(char), stderr);
+	fwrite(text, 1, sizeof(text) - sizeof(char), stderr);
 	return (1);
 }
