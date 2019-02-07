527f05da95ee0b0b4f2c216348c74d81a5dfc199
Sam Hermes <701@holbertonschool.com>
2019-02-07 15:52:52 +0000

    Adding second task: getting last digit of random number.

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point, prints last digit of random number at compares it to 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit, n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	}
+	else if (digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	}
+	else if (digit < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,6 @@
 
 ## Positive Anything Is Better Than Negative Nothing
 Printing whether a random number is positive, negative, or zero using `printf` and conditional blocks.
+
+## The Last Digit
+Printing the last digit of a random number and comparing it to five.
