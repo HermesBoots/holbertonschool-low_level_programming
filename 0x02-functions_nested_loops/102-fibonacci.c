b36d8af5b7283def30f7e7758c93a90476188929
Sam Hermes <701@holbertonschool.com>
2019-02-12 15:45:15 +0000

    Adding fifteenth task: printing 50 fibonnaci numbers.

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point, prints first 50 Fibonnaci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int current = 1, new, previous = 1;
+
+	for (i = 0; i < 49; i++)
+	{
+		printf("%lu, ", new = current + previous);
+		previous = current;
+		current = new;
+	}
+	printf("%lu\n", current + previous);
+	return (0);
+}
