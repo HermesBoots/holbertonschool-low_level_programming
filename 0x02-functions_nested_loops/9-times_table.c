3079ee08bf68978c15b4b518736f7f91172ed5f8
Sam Hermes <701@holbertonschool.com>
2019-02-12 16:23:44 +0000

    Messed with tenth task to try to satisfy checker.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,16 @@ void times_table(void)
 
 	for (row = 0; row < 10; row++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (col = 1; col < 10; col++)
+		for (col = 0; col < 10; col++)
 		{
 			product = row * col;
-			if (product < 10)
-				_putchar(' ');
-			else
-				_putchar(product / 10 + '0');
+			if (col > 0)
+			{
+				if (product < 10)
+					_putchar(' ');
+				else
+					_putchar(product / 10 + '0');
+			}
 			_putchar(product % 10 + '0');
 			if (col < 9)
 			{
