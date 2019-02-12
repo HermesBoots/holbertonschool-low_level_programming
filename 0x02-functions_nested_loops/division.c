c9cb0d6a817cae37965ee38f4d8c7fdf23fdb3be
Sam Hermes <701@holbertonschool.com>
2019-02-12 13:16:09 +0000

    Progress on division.

--- 0x02-functions_nested_loops/division.c
+++ 0x02-functions_nested_loops/division.c
@@ -4,6 +4,7 @@
 do { \
 	int den_96_1, den_96_2, den_96_3, den_96_4, den_96_5, den_96_6; \
 	int num_96_1, num_96_2, num_96_3, num_96_4, num_96_5, num_96_6; \
+	int quo_96_1, quo_96_2, quo_96_3, quo_96_4, quo_96_5, quo_96_6; \
 	int rem_96_1, rem_96_2, rem_96_3, rem_96_4, rem_96_5; \
 \
 	den_96_1 = (op2_hi) >> 16; \
@@ -19,61 +20,86 @@ do { \
 	num_96_5 = (op1_lo) >> 16; \
 	num_96_6 = (op1_lo) & 0xFFFF; \
 \
-	(op1_hi) = ((num_96_1 << 16) | num_96_2) / den_96_1 << 16; \
-	printf("%x %x %x %x\n", num_96_1, num_96_2, den_96_1, op1_hi); \
+	quo_96_1 = ((num_96_1 << 16) | num_96_2) / den_96_1; \
+	printf("%x %x %x %x\n", num_96_1, num_96_2, den_96_1, quo_96_1); \
 	rem_96_1 = ((num_96_1 << 16) | num_96_2) % den_96_1; \
-	(op1_hi) &= ( \
+	quo_96_2 = ( \
 		((rem_96_1 << 16) | num_96_3) - \
-		((op1_hi) >> 16) * den_96_2 \
-	) / den_96_1 & 0xFFFF; \
+		quo_96_1 * den_96_2 \
+	) / den_96_1; \
 	rem_96_2 = ( \
 		((rem_96_1 << 16) | num_96_3) - \
-		((op1_hi) >> 16) * den_96_2 \
+		quo_96_1 * den_96_2 \
 	) % den_96_1; \
-	op1_mid = ( \
+	quo_96_3 = ( \
 		((rem_96_2 << 16) | num_96_4) - \
-		((op1_hi) & 0xFFFF) * den_96_2 - \
-		((op1_hi) >> 16) * den_96_3 \
-	) / den_96_1 << 16; \
+		quo_96_2 * den_96_2 - \
+		quo_96_1 * den_96_3 \
+	) / den_96_1; \
 	rem_96_3 = ( \
 		((rem_96_2 << 16) | num_96_4) - \
-		((op1_hi) & 0xFFFF) * den_96_2 - \
-		((op1_hi) >> 16) * den_96_3 \
+		quo_96_2 * den_96_2 - \
+		quo_96_1 * den_96_3 \
 	) % den_96_1; \
-	op1_mid &= ( \
+	quo_96_4 = ( \
 		((rem_96_3 << 16) | num_96_5) - \
-		((op1_mid) >> 16) * den_96_2 - \
-		((op1_hi) & 0xFFFF) * den_96_3 - \
-		((op1_hi) >> 16) * den_96_4 \
-	) / den_96_1 & 0xFFFF; \
+		quo_96_3 * den_96_2 - \
+		quo_96_2 * den_96_3 - \
+		quo_96_1 * den_96_4 \
+	) / den_96_1; \
 	rem_96_4 = ( \
 		((rem_96_3 << 16) | num_96_5) - \
-		((op1_mid) >> 16) * den_96_2 - \
-		((op1_hi) & 0xFFFF) * den_96_3 - \
-		((op1_hi) >> 16) * den_96_4 \
+		quo_96_3 * den_96_2 - \
+		quo_96_2 * den_96_3 - \
+		quo_96_1 * den_96_4 \
 	) % den_96_1; \
-	op1_lo = ( \
+	quo_96_5 = ( \
 		((rem_96_4 << 16) | num_96_6) - \
-		((op1_mid) & 0xFFFF) * den_96_2 - \
-		((op1_mid) >> 16) * den_96_3 - \
-		((op1_hi) & 0xFFFF) * den_96_4 - \
-		((op1_hi) >> 16) * den_96_5 \
-	) / den_96_1 << 16; \
+		quo_96_4 * den_96_2 - \
+		quo_96_3 * den_96_3 - \
+		quo_96_2 * den_96_4 - \
+		quo_96_1 * den_96_5 \
+	) / den_96_1; \
 	rem_96_5 = ( \
 		((rem_96_4 << 16) | num_96_6) - \
-		((op1_mid) & 0xFFFF) * den_96_2 - \
-		((op1_mid) >> 16) * den_96_3 - \
-		((op1_hi) & 0xFFFF) * den_96_4 - \
-		((op1_hi) >> 16) * den_96_5 \
+		quo_96_4 * den_96_2 - \
+		quo_96_3 * den_96_3 - \
+		quo_96_2 * den_96_4 - \
+		quo_96_1 * den_96_5 \
 	) % den_96_1; \
-	op1_lo &= ( \
+	quo_96_6 = ( \
 		(rem_96_5 << 16) - \
-		((op1_lo) >> 16) * den_96_2 - \
-		((op1_mid) & 0xFFFF) * den_96_3 - \
-		((op1_mid) >> 16) * den_96_4 - \
-		((op1_hi) & 0xFFFF) * den_96_5 - \
-		((op1_hi) >> 16) * den_96_6 \
-	) / den_96_1 & 0xFFFF; \
+		quo_96_5 * den_96_2 - \
+		quo_96_4 * den_96_3 - \
+		quo_96_3 * den_96_4 - \
+		quo_96_2 * den_96_5 - \
+		quo_96_1 * den_96_6 \
+	) / den_96_1; \
+\
+	printf("quotient: %d %d %d %d %d %d\n", quo_96_1, quo_96_2, quo_96_3, quo_96_4, quo_96_5, quo_96_6); \
+	(op1_hi) = quo_96_1; \
+	if (quo_96_2 < 0) \
+	{ \
+		(op1_hi)--; \
+		(op1_mid) = 0xFFFF0000 + (quo_96_2 << 16) + 0x10000; \
+	} \
+	else \
+	{ \
+		(op1_mid) = quo_96_2 << 16; \
+	} \
+	(op1_mid) += quo_96_3; \
+	if (quo_96_4 < 0) \
+	{ \
+		(op1_mid)--; \
+		(op1_lo) = 0xFFFF0000 + (quo_96_4 << 16) + 0x10000; \
+	} \
+	else \
+	{ \
+		(op1_lo) = quo_96_4 << 16; \
+	} \
+	(op1_lo) += quo_96_5; \
+	if (quo_96_6 < 0) \
+		(op1_lo)--; \
 } while (0)
 
 /**
