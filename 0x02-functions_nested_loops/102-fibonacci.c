75a55d60d3e1deb24697a70c3fda3091afd5641f
Sam Hermes <701@holbertonschool.com>
2019-02-12 15:58:00 +0000

    Fixed extra sum in fifteenth task.

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 	int i;
 	unsigned long int current = 1, new, previous = 1;
 
-	for (i = 0; i < 47; i++)
+	for (i = 0; i < 49; i++)
 	{
 		printf("%lu, ", current);
 		new = previous + current;
 		previous = current;
 		current = new;
 	}
-	printf("%lu\n", current + previous);
+	printf("%lu\n", current);
 	return (0);
 }
