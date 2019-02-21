38d8ff888e98747515215303dad5cae87f09a819
Sam Hermes <701@holbertonschool.com>
2019-02-21 14:35:15 +0000

    Adding fourth task: recreating strcmp.

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: one of the strings
+ * @s2: the other string
+ *
+ * Return: 0 if strings match, negative if s1 is lower, positive if s2 is lower
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i] && s2[i]; i++)
+		;
+	if (s1[i] < s2[i])
+		return (i + 1);
+	else if (s2[i] < s1[i])
+		return (-i - 1);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -8,3 +8,6 @@ Concatenating strings but with a limit to control buffer overflows.
 
 ## `strncpy`
 Copying a string, but with a limit to control buffer overflows.
+
+## `strcmp`
+Comparing two strings.
