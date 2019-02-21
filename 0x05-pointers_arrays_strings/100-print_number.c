f856ff6d5037f25eb93544b8b4bdeca7bc73f097
Sam Hermes <701@holbertonschool.com>
2019-02-21 18:04:43 +0000

    Trying version of tenth task from earlier project.

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,28 +1,23 @@
 #include "holberton.h"
 
 /**
- * print_number - print an integer
- * @n: the integer
+ * print_number - print a number without printf
+ * @n: the number to print
  */
 void print_number(int n)
 {
-	int max = 1000000000;
-	int digit;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	int divisor, quotient;
 
 	if (n < 0)
 		_putchar('-');
-	while (max > 0)
+	for (divisor = 1000000000; divisor >= 10; divisor /= 10)
 	{
-		digit = n / max % 10;
-		digit = digit > 0 ? digit : -digit;
-		if (digit)
-			_putchar(digit + '0');
-		max /= 10;
+		quotient = n / divisor;
+		quotient = quotient > 0 ? quotient : -quotient;
+		if (quotient > 0)
+			_putchar(quotient % 10 + '0');
 	}
+	n = n % 10;
+	n = n > 0 ? n : -n;
+	_putchar(n + '0');
 }
