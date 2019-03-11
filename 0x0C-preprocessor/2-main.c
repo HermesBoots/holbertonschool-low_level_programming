83e7be5ac7fb3a27f34d50a4bb5352a6c6f01e12
Sam Hermes <701@holbertonschool.com>
2019-03-11 06:21:12 +0000

    Adding third task: printing source file name.

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints source file name
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts(__FILE__);
+	return (0);
+}
--- 0x0C-preprocessor/README.md
+++ 0x0C-preprocessor/README.md
@@ -5,3 +5,6 @@ Creating a simple macro.
 
 ## Pi
 Creating a macro for pi.
+
+## File Name
+Printing the name of a source file.
