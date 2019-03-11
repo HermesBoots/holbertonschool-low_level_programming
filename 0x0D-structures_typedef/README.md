a207a7c5316c53a04dfe3dd2fdbf3a12f3e73848
Sam Hermes <701@holbertonschool.com>
2019-03-11 06:40:22 +0000

    Adding first task: dog struct.

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,4 @@
+# C - Structures, `typedef`
+
+## Django
+Creating a dog structure.
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - represents a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
