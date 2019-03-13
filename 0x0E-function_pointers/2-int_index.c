d35635a353461a782d2616c812e308778945d35f
Sam Hermes <701@holbertonschool.com>
2019-03-13 16:40:28 +0000

    Adding argument validation to third task.

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#define NULL ((void *)0)
+
 /**
  * int_index - find a value within an array using comparison function
  * @array: pointer to array
@@ -10,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1)
+	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
