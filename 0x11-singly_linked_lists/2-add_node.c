b4035dedc2f3e9660815f821d924468b55245c0a
Sam Hermes <samuel.of.hermes@gmail.com>
2019-03-21 04:36:22 +0000

    Adding NULL checks to third task.

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -15,18 +15,29 @@ list_t *add_node(list_t **head, char const *str)
 	list_t *node;
 	size_t len;
 
-	copy = strdup(str);
-	node = malloc(sizeof(list_t));
-	if (copy == NULL || node == NULL)
-	{
-		free(copy);
-		free(node);
+	if (head == NULL)
 		return (NULL);
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		node->str = NULL;
+		node->len = 0;
 	}
-	for (len = 0; copy[len] != '\0'; len++)
-		;
-	node->str = copy;
-	node->len = len;
+	else
+	{
+		copy = strdup(str);
+		if (copy == NULL)
+		{
+			free(node);
+			return (NULL);
+		}
+		for (len = 0; copy[len] != '\0'; len++)
+			;
+		node->len = len;
+		node->str = copy;
+	}
 	node->next = *head;
 	*head = node;
 	return (node);
