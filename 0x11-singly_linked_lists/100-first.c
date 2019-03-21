5950b638c13a74388295fd0d5d8162d8e5ff5e9a
Sam Hermes <701@holbertonschool.com>
2019-03-21 17:35:35 +0000

    Adding function comment to sixth task.

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 
+/**
+ * setup - prints some text before main executes
+ */
 void __attribute__ ((constructor)) setup()
 {
 	printf("You're beat! and yet, you must allow,\n");
