21907c0a759eb66224a09ae2287f55d2bc20b80d
Sam Hermes <samuel.of.hermes@gmail.com>
2019-03-26 05:34:28 +0000

    Fixing handling of empty lists in tenth task.

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,24 +13,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node;
+	listint_t *node = NULL;
 
 	if (head == NULL)
 		return (NULL);
-	if (head != NULL || idx != 0)
+	if (*head != NULL)
 	{
 		idx--;
 		while ((*head)->next != NULL && idx--)
 			head = &(*head)->next;
 		if (idx != UINT_MAX)
 			return (NULL);
+		node = malloc(sizeof(*node));
+		if (node == NULL)
+			return (NULL);
+		node->n = n;
+		((int *)node)[1] = 0;
+		node->next = (*head)->next;
+		(*head)->next = node;
 	}
-	node = malloc(sizeof(*node));
-	if (node == NULL)
-		return (NULL);
-	node->n = n;
-	((int *)node)[1] = 0;
-	node->next = (*head)->next;
-	(*head)->next = node;
+	else if (idx == 0)
+	{
+		node = malloc(sizeof(*node));
+		if (node == NULL)
+			return (NULL);
+		node->n = n;
+		((int *)node)[1] = 0;
+		node->next = NULL;
+		*head = node;
+	}
 	return (node);
 }
