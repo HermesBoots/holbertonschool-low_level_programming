6ba88f50dcd14a2a3b4b2e895c0738beddb7f926
HermesBoots <samuel.of.hermes@gmail.com>
2019-06-19 19:59:36 +0000

    Include redundant lines in second quick sort

--- 0x1B-sorting_algorithms/107-quick_sort_hoare.c
+++ 0x1B-sorting_algorithms/107-quick_sort_hoare.c
@@ -23,8 +23,7 @@ void do_quick_sort2(int *array, size_t size, size_t start, size_t stop)
 			array[left] ^= array[right];
 			array[right] ^= array[left];
 			array[left] ^= array[right];
-			if (array[left] != array[right])
-				print_array(array, size);
+			print_array(array, size);
 			left++;
 		}
 		if (array[left] < pivot)
