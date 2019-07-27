2e4dcd03a16444981a2bde131530e867812c62c8
HermesBoots <samuel.of.hermes@gmail.com>
2019-07-27 01:18:14 +0000

    Check for null nodes in task 3

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -8,13 +8,13 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return;
 	if (tree->left != NULL)
 		binary_tree_delete(tree->left);
 	if (tree->right != NULL)
 		binary_tree_delete(tree->right);
 	if (tree->parent != NULL && tree->parent->left == tree)
 		tree->parent->left = NULL;
-	if (tree->parent != NULL && tree->parent->right == tree)
-		tree->parent->right = NULL;
 	free(tree);
 }
