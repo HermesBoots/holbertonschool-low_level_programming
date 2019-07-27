b6f1f8c1809d1cf6401757bdc8e1d6866126b006
HermesBoots <samuel.of.hermes@gmail.com>
2019-07-27 01:17:16 +0000

    Also remove pointers in parent nodes in task 3

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -12,5 +12,9 @@ void binary_tree_delete(binary_tree_t *tree)
 		binary_tree_delete(tree->left);
 	if (tree->right != NULL)
 		binary_tree_delete(tree->right);
+	if (tree->parent != NULL && tree->parent->left == tree)
+		tree->parent->left = NULL;
+	if (tree->parent != NULL && tree->parent->right == tree)
+		tree->parent->right = NULL;
 	free(tree);
 }
