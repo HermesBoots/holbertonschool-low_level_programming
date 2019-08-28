604bcb531010a41d4b3ff8ab8d39841b3fc05692
HermesBoots <701@holbertonschool.com>
2019-08-28 19:53:40 +0000

    Add task 12: jump search with a linked list

--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -11,3 +11,6 @@ Finding the big O space and time complexities of various algorithms.
 
 ## Jump Search
 Perform a jump search of a sorted array.
+
+## Jump Search in a Singly Linked List
+Using jump search with a singly-linked list to demonstrate its inefficiency.
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,22 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 
+/**
+ * struct listint_s - simple linked list nodes
+ * @n: value in the nodes
+ * @index: index in the list
+ * @next: pointer to next node in the list
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
